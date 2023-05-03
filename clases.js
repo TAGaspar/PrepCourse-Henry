7a6f792875b67b06cd1144001e1c719695346331
TAGaspar <tomasgaspar163@gmail.com>
2023-05-02 23:01:45 -0300

    clases y prototipos

---
+++ clases.js
@@ -0,0 +1,38 @@
+// function Auto(puertas, color, marca, año, ruedas) {
+//     this.puertas = puertas;
+//     this.color = color;
+//     this.marca = marca;
+//     this.año = año;
+//     this.ruedas = ruedas;
+
+//     this.informacion = function(){
+//         console.log("Este es un " + this.marca + " de color " + this.color)    }
+// }
+
+// var miPrimerAuto = new Auto(2, "rojo", "Ferrari", 2018, 4);
+
+// //console.log(miPrimerAuto);
+// //console.log(miPrimerAuto.marca);
+
+// miPrimerAuto.informacion();
+
+class Auto{
+    constructor(puertas, color, marca, año, ruedas){
+        this.puertas = puertas;
+        this.color = color;
+        this.marca = marca;
+         this.año = año;
+         this.ruedas = ruedas;
+    }
+
+    informacion(){
+        console.log("Este es un " + this.marca + " de color " + this.color);
+    }
+}
+
+var miSegundoAuto = new Auto(4, "Blanco", "Fiat", 2015, 4);
+
+// console.log(miSegundoAuto);
+// console.log(miSegundoAuto.marca);
+
+miSegundoAuto.informacion();
